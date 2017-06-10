6e6b0fcc4f815a8be91662c77b99d7b577eb15c0
damianedwards <damian@damianedwards.com>
2017-06-09 18:34:07 -0700

    Added Admin authz policy

--- src/FrontEnd/Pages/Index.cshtml.cs
+++ src/FrontEnd/Pages/Index.cshtml.cs
@@ -5,22 +5,29 @@ using System.Threading.Tasks;
 using ConferenceDTO;
 using FrontEnd.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FrontEnd.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly IApiClient _apiClient;
+        private readonly IAuthorizationService _authz;
 
-        public IndexModel(IApiClient apiClient)
+        public IndexModel(IApiClient apiClient, IAuthorizationService authz)
         {
             _apiClient = apiClient;
+            _authz = authz;
         }
 
         public IEnumerable<IGrouping<DayOfWeek?, IGrouping<DateTimeOffset?, SessionResponse>>> Sessions { get; set; }
 
+        public bool IsAdmin { get; set; }
+
         public async Task OnGet(int day = 0)
         {
+            IsAdmin = await _authz.AuthorizeAsync(User, "Admin");
+
             var sessions = await _apiClient.GetSessionsAsync();
 
             var firstDay = sessions.Min(s => s.StartTime?.Day);
--- src/FrontEnd/Startup.cs
+++ src/FrontEnd/Startup.cs
@@ -23,7 +23,7 @@ namespace FrontEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddRazorPagesOptions(options => options.AuthorizeFolder("/admin"));
+            services.AddMvc().AddRazorPagesOptions(options => options.AuthorizeFolder("/admin", "IsAdmin"));
 
             services.AddAuthentication(options =>
             {
