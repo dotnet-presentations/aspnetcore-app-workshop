e8ee94540c75de0f97ecb1f46dfdcf9d5fae8089
damianedwards <damian@damianedwards.com>
2017-06-11 13:59:04 +0200

    Tweak edit session form

--- src/FrontEnd/Pages/Admin/EditSession.cshtml.cs
+++ src/FrontEnd/Pages/Admin/EditSession.cshtml.cs
@@ -1,14 +1,15 @@
 using System.Threading.Tasks;
-using ConferenceDTO;
 using FrontEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FrontEnd.Pages.Models;
 
 namespace FrontEnd.Pages
 {
     public class EditSessionModel : PageModel
     {
         private readonly IApiClient _apiClient;
+
         public EditSessionModel(IApiClient apiClient)
         {
             _apiClient = apiClient;
@@ -19,12 +20,26 @@ namespace FrontEnd.Pages
 
         public async Task OnGet(int id)
         {
-            Session = await _apiClient.GetSessionAsync(id);
+            var session = await _apiClient.GetSessionAsync(id);
+            Session = new Session
+            {
+                ID = session.ID,
+                ConferenceID = session.ConferenceID,
+                TrackId = session.TrackId,
+                Title = session.Title,
+                Abstract = session.Abstract,
+                StartTime = session.StartTime,
+                EndTime = session.EndTime
+            };
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            // TODO: Validation errors etc
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
             await _apiClient.PutSessionAsync(Session);
 
             return RedirectToPage("/Session", new { id = Session.ID });
