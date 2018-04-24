4defe7a5bcdc052aad98d3cc779834ff681dc96d
Shayne Boyer <spboyer@live.com>
2018-04-24 08:28:34 -0400

    update master

--- save-points/3-Front-End-started/ConferencePlanner/BackEnd/Program.cs
+++ save-points/3-Front-End-started/ConferencePlanner/BackEnd/Program.cs
@@ -19,6 +19,7 @@ namespace BackEnd
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
--- save-points/3-Front-End-started/ConferencePlanner/FrontEnd/Program.cs
+++ save-points/3-Front-End-started/ConferencePlanner/FrontEnd/Program.cs
@@ -19,6 +19,7 @@ namespace FrontEnd
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
