2194cd26cecca4c793c57e05949573df962202e2
Shayne Boyer <spboyer@live.com>
2018-06-05 17:41:32 -0400

    updates for SPA references

--- save-points/8a-Adding-FrontEnd-Spa-final/ConferencePlanner/src/FrontEndSpa/Startup.cs
+++ save-points/8a-Adding-FrontEnd-Spa-final/ConferencePlanner/src/FrontEndSpa/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
