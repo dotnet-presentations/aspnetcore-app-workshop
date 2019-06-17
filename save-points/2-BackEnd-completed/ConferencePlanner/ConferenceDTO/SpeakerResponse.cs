6a41deef3f871be255727072e2736c46c009feae
David Fowler <davidfowl@gmail.com>
2019-06-16 22:52:22 -0700

    Changes to snapshot and session2

--- save-points/2-BackEnd-completed/ConferencePlanner/ConferenceDTO/SpeakerResponse.cs
+++ save-points/2-BackEnd-completed/ConferencePlanner/ConferenceDTO/SpeakerResponse.cs
@@ -6,7 +6,7 @@ namespace ConferenceDTO
 {
     public class SpeakerResponse : Speaker
     {
-        // TODO: Set order of JSON proeprties so this shows up last not first
+        // TODO: Set order of JSON properties so this shows up last not first
         public ICollection<Session> Sessions { get; set; } = new List<Session>();
     }
 }
