3125abc0b1268ef89fecabc1254d0da71a15c257
DamianEdwards <damian@damianedwards.com>
2021-10-28 14:34:57 -0700

    Move backend to top-level statements & WebApplication
    - Kept Startup class for now

--- src/BackEnd/Program.cs
+++ src/BackEnd/Program.cs
@@ -1,20 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-namespace BackEnd
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var startup = new Startup(builder.Configuration);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webHostBuilder =>
-                {
-                    webHostBuilder.UseStartup<Startup>();
-                });
-    }
-}
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+startup.Configure(app, app.Environment);
+
+app.Run();
