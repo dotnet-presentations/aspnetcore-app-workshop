731a4094f97dd4683a4af33805960bb2c037430c
DamianEdwards <damian@damianedwards.com>
2021-10-28 14:48:36 -0700

    Remove Startup from BackEnd
    - Use implicit usings

--- src/BackEnd/Program.cs
+++ src/BackEnd/Program.cs
@@ -1,10 +1,35 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
-var startup = new Startup(builder.Configuration);
+services.AddSqlServer<ApplicationDbContext>("name=DefaultConnection")
+        .AddDatabaseDeveloperPageExceptionFilter();
 
-startup.ConfigureServices(builder.Services);
+services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+
+services.AddHealthChecks()
+        .AddDbContextCheck<ApplicationDbContext>();
+
+services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new () { Title = "Conference Planner API", Version = "v1" });
+});
 
 var app = builder.Build();
-startup.Configure(app, app.Environment);
+
+app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Conference Planner API v1");
+});
+
+app.MapControllers();
+app.MapHealthChecks("/health");
+app.MapFallback(() => Results.Redirect("/swagger"));
 
 app.Run();
